73d0a4b47ea7ff705e31ea22a36a6199c1e88022
XtraPower98 <36510023+XtraPower98@users.noreply.github.com>
2018-05-03 00:30:53 +0100

    attempt1

--- src/myinterpreter.hs
+++ src/myinterpreter.hs
@@ -102,15 +102,12 @@ module Main where
         let tableNames = extractTableNames (pTree)
         allContents <- extractContents readContents tableNames
         let allTables = fmap buildTable allContents
-
         -- For future implementation where contentA-N and allTables is built dynamically.
         let bigTable = crossMulti(allTables)
         let answer = executeQuery (bigTable) (pTree)
         let output = readyOutput ( extractOutput (orderTable (lhsVar) (answer)))
         putStr("_____________________")
-        mapM_ putStrLn output      
-
-                                         
+        mapM_ putStrLn output                                    
     {-==============================================================================-}
     {-============================== SYNTAX CHECKER ================================-}
     {-==============================================================================-}
@@ -132,8 +129,6 @@ module Main where
     checkSyntax (Evaluate (VarSingle String) (V Variables)) = True
     checkSyntax (Evaluate (VarSingle String) (ExistentialSingle Variables Query)) = True
     -}
-
-  
     {-==============================================================================-}
     {-=============================== CSV EXTRACTION ===============================-}
     {-==============================================================================-}
@@ -155,8 +150,6 @@ module Main where
         table <- f tableName 
         rest <- extractContents f (xs)
         return $! table : rest
-
-
     
     removeDups :: [[VarNode]] -> [[VarNode]]
     removeDups [] = [[]]
@@ -165,12 +158,10 @@ module Main where
     removeDups' :: [[VarNode]] -> [VarNode]
     removeDups' [] = []
     removeDups' (x:xs) =  [head x] ++ removeDups' xs
-    
 
     {-==============================================================================-}
     {-================================= BUILDING ===================================-}
-    {-==============================================================================-}
-    
+    {-==============================================================================-} 
 
     buildParseTree :: Exp -> ParseTree
     buildParseTree (Evaluate (vars) (query)) = Marker (traverseDFVar(buildVarTree(vars)))  (buildOpTree(query))
@@ -180,8 +171,6 @@ module Main where
     buildVarTree (VarSingle strName) = SingleNode (Vari ("*") ("*") (strName))
     buildVarTree (Comma (string) remVars) = CommaNode (Vari ("*") ("*") (string)) (buildVarTree remVars)
 
-
-    
     buildOpTree :: Query -> OpTree 
     buildOpTree (Conjunction querA querB) = ConjunctionNode (buildOpTree querA) (buildOpTree querB)
     buildOpTree (Relation tblN varis) = RelationNode (tblN) (assignVarTreeLoc (buildVarTree varis) (tblN) )
@@ -189,13 +178,9 @@ module Main where
     buildOpTree (Equality querA querB) = (EquateNode (varToOpTree(buildVarTree(queryToVariables(querA)))) (varToOpTree(buildVarTree(queryToVariables(querB)))))
     buildOpTree (V varis) = VarOp (buildVarTree(varis))
     buildOpTree (ExistentialSingle vTree oTree) = (ExistVar (buildVarTree(vars)) (buildOpTree (quer))) --(EXIS REFORMAT)
-    buildOpTree _ = EmptyOT (Main.Nothing)
-    
+    buildOpTree _ = EmptyOT (Main.Nothing)    
     --buildRelationalTree :: String -> VarTree -> OpTree
     --buildRelationalTree tblName vTree = (RelationNode (tblName) (assignVarTreeLoc (vTree) (tblName)))
-    
-
-
 
     {-==============================================================================-}
     {-============================== TABLE OPERATIONS ==============================-}
